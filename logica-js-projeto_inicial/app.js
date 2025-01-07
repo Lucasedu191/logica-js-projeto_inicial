2d62410cb3e3c78c392b83841a51b5736cdf3fcc
lucasedu191 <lucasatletico@hotmail.com>
2025-01-06 22:02:59 -0300

    primeiro commit

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,23 @@
+alert('Boas vindas ao jogo de numero secreto');
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1; 
+
+while( chute != numeroSecreto){
+    chute = prompt('escolha um numero entre 1 e 10');
+    if( chute == numeroSecreto ){
+        break;
+    }else{
+
+        if(chute < numeroSecreto){
+            alert(`o numero secreto é maior que ${chute}`);
+        }else{
+            alert(`o numero secreto é menor que ${chute}`);
+        }
+        tentativas++;
+    }
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`acertou o numero ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
